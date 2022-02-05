68da2cd6c77dc4a9ca605d9f9c061bce7e2c01e3
Karthik v <56793779+karthik200116@users.noreply.github.com>
2022-02-06 01:23:21 +0530

    Create LinkedList.py

---
+++ LinkedList.py
@@ -0,0 +1,33 @@
+class Node:
+	def __init__(self, data=None, next=None):
+		self.data = data
+		self.next = next
+	
+	def print_node(self):
+		print(self.data)
+	
+	
+class LinkedList:
+	def __init__(self):
+		self.head = None
+	
+	def append_node(self, data):
+		if not self.head :
+			self.head = Node(data)
+			return
+		current = self.head
+		while current.next:
+			current = current.next
+		current.next = Node(data)
+	def print_list(self):
+		node = self.head
+		while node is not None:
+			print(node.data)
+			node = node.next
+
+			
+if __name__ = '__main__':
+	linked_list = LinkedList()
+	linked_list.append_node('Monday')
+	linked_list.append_node('Tuesday')
+	linekd_list.print_list()
